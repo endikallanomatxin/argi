ddcc23526d7ade12fd592ffe34b2af3202898e1c
Endika Llano Matxin <endikallano@gmail.com>
2025-10-01 21:46:49 +0200

    Refresh context generation scripts

--- compiler/context.sh
+++ compiler/context.sh
@@ -1,58 +1,55 @@
 #!/bin/bash
-# Detectar el comando para copiar al portapapeles según el sistema operativo
-if [[ "$(uname)" == "Darwin" ]]; then
-    clipboard_cmd="pbcopy"
-elif [[ "$(uname)" == "Linux" ]]; then
-    # Se asume que xclip está instalado en Linux
-    clipboard_cmd="xclip -selection clipboard"
-elif [[ "$(uname)" =~ MINGW|CYGWIN ]]; then
-    clipboard_cmd="clip"
-else
-    echo "Sistema operativo no soportado" >&2
-    exit 1
+# Crear el contenido a copiar
+echo "----- ls -T -----"
+ls -T
+
+# Incluir readme.md
+if [ -f "README.md" ]; then
+    echo ""
+    echo "----- README.md -----"
+    cat "README.md"
 fi
 
-# Crear el contenido a copiar
-{
-    echo "----- ls -T -----"
-    ls -T
-
-    # Incluir readme.md
-    if [ -f "README.md" ]; then
-        echo ""
-        echo "----- README.md -----"
-        cat "README.md"
-    fi
-
-    # Incluir todos los archivos en src excepto .zig-cache
-    if [ -d "src" ]; then
-        for f in src/*; do
-            if [ "$(basename "$f")" = ".zig-cache" ]; then
-                continue
-            fi
-            if [ -f "$f" ]; then
-                echo ""
-                echo "----- $f -----"
-                cat "$f"
-            fi
-        done
-    fi
-
-    # Incluir test.rg
-    if [ -f "test.rg" ]; then
-        echo ""
-        echo "----- test.rg -----"
-        cat "test.rg"
-    fi
-
-    # Incluir build.zig y build.zig.zon
-    for file in build.zig build.zig.zon; do
-        if [ -f "$file" ]; then
+# Incluir todos los archivos en src excepto .zig-cache
+if [ -d "src" ]; then
+    for f in src/*; do
+        if [ "$(basename "$f")" = ".zig-cache" ]; then
+            continue
+        fi
+        if [ -f "$f" ]; then
             echo ""
-            echo "----- $file -----"
-            cat "$file"
+            echo "----- $f -----"
+            cat "$f"
         fi
     done
-} | $clipboard_cmd
+fi
 
-echo "Contenido copiado al portapapeles."
+# Incluir test.rg
+if [ -f "test.rg" ]; then
+    echo ""
+    echo "----- test.rg -----"
+    cat "test.rg"
+fi
+
+# Incluir todos los archivos en test excepto .zig-cache
+if [ -d "tests" ]; then
+    for f in tests/*; do
+        if [ "$(basename "$f")" = ".zig-cache" ]; then
+            continue
+        fi
+        if [ -f "$f" ]; then
+            echo ""
+            echo "----- $f -----"
+            cat "$f"
+        fi
+    done
+fi
+
+# Incluir build.zig y build.zig.zon
+for file in build.zig build.zig.zon; do
+    if [ -f "$file" ]; then
+        echo ""
+        echo "----- $file -----"
+        cat "$file"
+    fi
+done
